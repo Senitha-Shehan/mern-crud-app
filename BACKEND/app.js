1a153341d943c1a5c34af733aef55f9dfd016607
Senitha-Shehan <senitha200109@gmail.com>
2024-11-16 19:53:49 +0530

    Create DB Connection

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,1 +1,20 @@
-console.log("hi");
+//Pass -  MqrHclf5DeW04UtC
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//middleware
+
+app.use("/", (req, res, next) => {
+    res.send("It is Working");
+});
+
+mongoose.connect("mongodb+srv://admin:MqrHclf5DeW04UtC@cluster0.kvzm4.mongodb.net/")
+.then(() => console.log("Database Connected"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err) => console.log(err));
+
